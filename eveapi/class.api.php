2abfeae61e0be23c4a13e63548448ea33afabf6e
eldstrom <eldstrom@01fdbf8f-2751-0410-a0c9-d59e30f57b93>
2008-11-13 12:44:37 +0000

    removed cert retrieval function
    git-svn-id: http://svn.code.sf.net/p/eve-apiphp/code/trunk@115 01fdbf8f-2751-0410-a0c9-d59e30f57b93

--- eveapi/class.api.php
+++ eveapi/class.api.php
@@ -1263,27 +1263,7 @@ class Api
 		return $contents;
 	}
 
-	public function getCertificates( $timeout = null)
-	{
-		if ($timeout && !is_numeric($timeout))
-		{
-			if ($this->debug)
-			{
-				$this->addMsg("Error","getCertifcates: Non-numeric value of timeout param, reverting to default value");
-			}
-			$timeout = null;
-		}
-		
-		$cachePath = array();
-	 	$cachePath[0] = 'userID';
-		$cachePath[1] = 'characterID';
-		
-		$contents = $this->retrieveXml("/char/Certificates.xml.aspx", $timeout, $cachePath);
-		
-		return $contents;
-	}
-
-
+	
 	public function getMarketOrders($corp = false, $timeout = null)
 	{
 		if ($timeout && !is_numeric($timeout))
