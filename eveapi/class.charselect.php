637c83cb7f945c8df9c18fd99847305ac79e07ee
eldstrom <eldstrom@01fdbf8f-2751-0410-a0c9-d59e30f57b93>
2008-12-03 12:58:30 +0000

    git-svn-id: http://svn.code.sf.net/p/eve-apiphp/code/trunk@127 01fdbf8f-2751-0410-a0c9-d59e30f57b93

--- eveapi/class.charselect.php
+++ eveapi/class.charselect.php
@@ -20,7 +20,7 @@
 	along with PHP Api Lib.  If not, see <http://www.gnu.org/licenses/>.
 **************************************************************************/
 // class.generic.php was renamed to be in line with new naming conventions - this file allows for legacy code to continue working
-require_once('./class.characters.php'); 
+require_once('class.characters.php'); 
 
 class CharSelect
 {
