1f9707a6f3a4de52b099a8e352cf0e4544448a82
sbehrens <sbehrens@01fdbf8f-2751-0410-a0c9-d59e30f57b93>
2009-01-19 12:39:04 +0000

    git-svn-id: http://svn.code.sf.net/p/eve-apiphp/code/trunk@173 01fdbf8f-2751-0410-a0c9-d59e30f57b93

--- eveapi/class.marketorders.php
+++ eveapi/class.marketorders.php
@@ -1,22 +1,22 @@
 <?php
 /**************************************************************************
-	PHP Api Lib MarketOrders Class
+	Ale API Library for EvE MarketOrders Class
 	Copyright (c) 2008 Dustin Tinklin
 
-	This file is part of PHP Api Lib.
+	This file is part of Ale API Library for EvE.
 
-	PHP Api Lib is free software: you can redistribute it and/or modify
+	Ale API Library for EvE is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Lesser General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
 
-	PHP Api Lib is distributed in the hope that it will be useful,
+	Ale API Library for EvE is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU Lesser General Public License for more details.
 
 	You should have received a copy of the GNU Lesser General Public License
-	along with PHP Api Lib.  If not, see <http://www.gnu.org/licenses/>.
+	along with Ale API Library for EvE.  If not, see <http://www.gnu.org/licenses/>.
 **************************************************************************/
 
 class MarketOrders
